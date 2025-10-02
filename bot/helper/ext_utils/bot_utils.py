c1e7385e3f03e665637debd4b2900e85675a0367
DOWNLOADER ZONE <downloaderzone1@gmail.com>
2025-10-02 14:37:47 +0000

    Fix Values Error

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -305,3 +305,10 @@ def loop_thread(func):
         return future.result() if wait else future
 
     return wrapper
+
+
+def safe_int(value, default=0):
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        return default
